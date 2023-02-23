4fe1d6f05eafbad9636cee2b41970d36b0b6032c
userFANwei <sevenfan716@163.com>
2023-02-23 21:20:14 +0800

    Site updated: 2023-02-23 21:20:14

---
+++ js/light.js
@@ -0,0 +1,5 @@
+inject:
+  head:
+    - <link rel="stylesheet" href="/css/custom.css" media="defer" onload="this.media='all'">
+  bottom: 
+    - <script defer src="/js/light.js"></script> # 霓虹灯(必须defer否则有时候会不生效)
